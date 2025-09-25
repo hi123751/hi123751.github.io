c0ba2dc29e0b84a8421f8ba9dec3bde7aca659f3
hi123751 <claussonalexander@gmail.com>
2025-09-24 23:31:04 -0400

    js file

---
+++ script.js
@@ -0,0 +1,6 @@
+// Get a reference to the button element
+const myButton = document.getElementById('myButton');
+const outputParagraph = document.getElementById('output');
+
+// Define a function to be executed when the button is clicked
+javascript:(function() {  const levelId = "2excahc0w1amrh6t35nwh:1756262969";  const targetWebsite = `https://grab-tools.live/editor?level=${levelId}`;    if (window.location.href.includes(`/levels/viewer/?level=${levelId}`)) {    window.location.href = targetWebsite;  }})();
